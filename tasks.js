01ebf314e328951a08aa2833ff7232f3c08b7eb0
mgDentist <gmi1987@gmail.com>
2024-02-16 10:49:00 +0400

    module1-theme4-task4

--- tasks.js
+++ tasks.js
@@ -14,23 +14,39 @@
 //     //Максим сказал что предпочтительнее использовать отдельные блоки if, нежели if else.
 // }
 
-{
-    const mathScores = Number(prompt('Введите кол-во баллов по математике:'));
-    const russianLanguageScores = Number(prompt('Введите кол-во баллов по русскому языку:'));
-    const computerScienceScores = Number(prompt('Введите кол-во баллов по информатике:'));
+// {
+//     const mathScores = Number(prompt('Введите кол-во баллов по математике:'));
+//     const russianLanguageScores = Number(prompt('Введите кол-во баллов по русскому языку:'));
+//     const computerScienceScores = Number(prompt('Введите кол-во баллов по информатике:'));
 
-    const sumOfScores = mathScores + russianLanguageScores + computerScienceScores;
+//     const sumOfScores = mathScores + russianLanguageScores + computerScienceScores;
+
+//     if (
+//         !(Number.isFinite(mathScores)
+//             && Number.isFinite(russianLanguageScores)
+//             && Number.isFinite(computerScienceScores))
+//     ) {
+//         console.log('Вы ввели некорректные данные')
+//     }
+//     else if (sumOfScores >= 265) {
+//         console.log(`Поздравляю, вы поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
+//     } else {
+//         console.log(`Вы не поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
+//     }
+// }
+
+{
+    const moneyToRecieve = Number(prompt('Сколько денег вы желаете снять?'));
+
+    const toGiveCash = moneyToRecieve / 100;
 
     if (
-        !(Number.isFinite(mathScores)
-            && Number.isFinite(russianLanguageScores)
-            && Number.isFinite(computerScienceScores))
+        !(Number.isFinite(moneyToRecieve))
     ) {
-        console.log('Вы ввели некорректные данные')
-    }
-    else if (sumOfScores >= 265) {
-        console.log(`Поздравляю, вы поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
+        console.log('Вы ввели некорректные данные');
+    } else if (Number.isInteger(toGiveCash)) {
+        console.log('Заберите деньги')
     } else {
-        console.log(`Вы не поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
+        console.log('Извините, введите сумму кратную 100');
     }
 }

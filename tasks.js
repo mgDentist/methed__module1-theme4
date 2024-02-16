8b735ee86d969fbcb5d3be325c772adc06517fe6
mgDentist <gmi1987@gmail.com>
2024-02-16 10:10:46 +0400

    module1-theme4-task2

---
+++ tasks.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+    const rain = Math.round(Math.random());
+
+    if (rain === 1) {
+        console.log('Пошёл дождь. Возьмите зонт!');
+    }
+
+    if (rain === 0) {
+        console.log('Дождя нет!');
+    }
+
+    //Максим сказал что предпочтительнее использовать отдельные блоки if, нежели if else.
+}

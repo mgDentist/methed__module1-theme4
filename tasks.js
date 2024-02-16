fe6f54490f6ad99a8ceb9cb6bc2c9cac5a8a3dc2
mgDentist <gmi1987@gmail.com>
2024-02-16 10:34:02 +0400

    module1-theme4-task3

--- tasks.js
+++ tasks.js
@@ -1,15 +1,36 @@
 'use strict';
 
+// {
+//     const rain = Math.round(Math.random());
+
+//     if (rain === 1) {
+//         console.log('Пошёл дождь. Возьмите зонт!');
+//     }
+
+//     if (rain === 0) {
+//         console.log('Дождя нет!');
+//     }
+
+//     //Максим сказал что предпочтительнее использовать отдельные блоки if, нежели if else.
+// }
+
 {
-    const rain = Math.round(Math.random());
+    const mathScores = Number(prompt('Введите кол-во баллов по математике:'));
+    const russianLanguageScores = Number(prompt('Введите кол-во баллов по русскому языку:'));
+    const computerScienceScores = Number(prompt('Введите кол-во баллов по информатике:'));
 
-    if (rain === 1) {
-        console.log('Пошёл дождь. Возьмите зонт!');
+    const sumOfScores = mathScores + russianLanguageScores + computerScienceScores;
+
+    if (
+        !(Number.isFinite(mathScores)
+            && Number.isFinite(russianLanguageScores)
+            && Number.isFinite(computerScienceScores))
+    ) {
+        console.log('Вы ввели некорректные данные')
     }
-
-    if (rain === 0) {
-        console.log('Дождя нет!');
+    else if (sumOfScores >= 265) {
+        console.log(`Поздравляю, вы поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
+    } else {
+        console.log(`Вы не поступили на бюджет! Ваш общий балл: ${sumOfScores}`);
     }
-
-    //Максим сказал что предпочтительнее использовать отдельные блоки if, нежели if else.
 }
